b78f87a2348311fb748e18b06906f4d4d86a877f
josmel9319 <44248402+josmel9319@users.noreply.github.com>
2018-11-27 19:23:29 -0500

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ECPI is almost over for me!';
+  reminder1 = 'Call Mom';
+  reminder2 = 'Get Milk';
+  reminder3 = 'Interview';
+}
